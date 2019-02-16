7f2cae919b8dc6582f9bc6998a7b992845a45a2b
ausdev18 <36680342+ausdev18@users.noreply.github.com>
2019-02-16 11:40:32 +0000

    Update to_title_case.js
    it now works

--- to_title_case.js
+++ to_title_case.js
@@ -1,18 +1,17 @@
 function titleCase(str) {
-
   let lc = str.toLowerCase();
-
   let sentence = lc.split(" ");
-
-  for (let i = 0; i < sentence.length; i++) {
-    let firstLetter = sentence[i].match(/./);
-    let end = sentence[i].replace(/(.)/, convert(firstLetter));
-    return end;
-  }
-
+  let arr = [];
 
   function convert(a) {
     return a[0].toUpperCase();
   }
+  
+  for (let i = 0; i < sentence.length; i++) {
+    let firstLetter = sentence[i].match(/./);
+    arr.push(sentence[i].replace(firstLetter, convert(firstLetter)));
+  }
+
+return arr.join(" ");
 
 titleCase("I'm a little tea pot");
