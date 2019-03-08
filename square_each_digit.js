92d6d8ac3a78b6c142fce05902022a274ae4eb0e
Christopher King <36680342+seekingcode18@users.noreply.github.com>
2019-03-08 18:15:00 +0000

    Update and rename square_each_digit to square_each_digit.js

--- square_each_digit.js
+++ square_each_digit.js
@@ -6,3 +6,10 @@ squared = arr.map(num => num * num)
 return Number(squared.join(''))
 
 }
+
+
+//single line version
+
+//function squareDigits(num) {
+//return Number(JSON.stringify(num).split('').map(num => num * num).join(''))
+//}
